34f7a2d815ab3ba52e8f33a2df81362b8c3ee8f3
Tal Gavriel <talgavriel1@gmail.com>
2020-09-30 01:21:49 +0300

    add point to binary tree and print

---
+++ NearestPoint/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearestPoint
+{
+    public class Point : IComparable<Point>, IEquatable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int CompareTo(Point other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var xComparison = X.CompareTo(other.X);
+            if (xComparison != 0) return xComparison;
+            return Y.CompareTo(other.Y);
+        }
+
+        public bool Equals(Point other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Point) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+}
---
+++ NearestPoint/Program.cs
@@ -0,0 +1,67 @@
+﻿// Tal Gavriel, ID: 209209808
+// Mor Cohen, ID: 315825356
+
+using System;
+using System.Collections.Generic;
+
+namespace NearestPoint
+{
+    class Program
+    {
+        private static readonly BinarySearchTree<Point> BinarySearchTree = new BinarySearchTree<Point>();
+        private static List<Point> _listOfPoints = new List<Point>();
+        private static readonly Random Random = new Random();
+        private static readonly object SyncLock = new object();
+        public static void Main(string[] args)
+        {
+            BuildTree();
+            _listOfPoints = BinarySearchTree.GetNodesInOrder(BinarySearchTree.Root, new List<Point>());
+            printPoints();
+            Console.ReadLine();
+        }
+
+        public static void BuildTree()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Point newPoint = GetRandomPoint();
+                BinarySearchTree.Insert(newPoint);
+            }
+        }
+
+        public static void printPoints()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    Point newPoint = new Point(i,j);
+
+                    if (_listOfPoints.Contains(newPoint))
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+       
+
+        public static Point GetRandomPoint()
+        {
+            lock (SyncLock)
+            {
+                 int x = Random.Next(0, 100);
+                 int y = Random.Next(0, 100);
+                 return new Point(x, y);
+
+            }
+        }
+    }
+}
